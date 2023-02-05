c1a2176f80159af3dadc6fa59ea666a1c56e782a
jungiIssei <jungissei@gmail.com>
2023-02-06 08:33:27 +0900

    --

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_URL_PREFIX === "prod" ? "/MUI-SandBox" : "",
-  basePath: process.env.NEXT_PUBLIC_URL_PREFIX === "prod" ? "/MUI-SandBox" : "",
+  assetPrefix: '/MUI-SandBox',
+  basePath: '/MUI-SandBox',
 };
--- src/addUrlPrefix.ts
+++ src/addUrlPrefix.ts
@@ -4,7 +4,5 @@
  * @see https://penpen-dev.com/blog/nextjs-subdirectory/
  */
 export default function addUrlPrefix(url: string) {
-  return process.env.NEXT_PUBLIC_URL_PREFIX === "prod"
-    ? `/MUI-SandBox${url}`
-    : url;
+  return `/MUI-SandBox${url}`;
 }
